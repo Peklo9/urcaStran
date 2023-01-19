504c6f1b99e0d69b7bbf5663c3d5e420a5410a7d
Peklo9 <109766795+Peklo9@users.noreply.github.com>
2023-01-19 22:50:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
 
-
-
-
 const tip = document.querySelector('.tip')
-//const dodaj = document.querySelector('.dodaj')
 const gumbi = document.querySelector('.okvirGumbi')
 const stanjePrikaz = document.querySelector('.stanje')
 const obvestiloText = document.querySelector('.obvestiloText')
 const obvestilo = document.querySelector('.obvestilo')
-const gumbZapri = document.querySelector('.gumbZapri')
+const gumbZapriObvestilo = document.querySelector('.gumbZapriObvestilo')
 const tabela = document.querySelector('.tabela')
 const mesci = document.querySelector('.mesci')
 const zapriTabelo = document.querySelector('.zapriTabelo')
 const tabelaOkvir = document.querySelector('.tabelaOkvir')
 const prikaziTabelo= document.querySelector('.prikaziTabelo')
 const gumbPrihod= document.querySelector('.gumbPrihod')
-const gumbaBrisanje= document.querySelector('.gumbaBrisanje')
+const gumbOdhod= document.querySelector('.gumbOdhod')
 const okvirBrisiPrihOdh = document.querySelector('.okvirBrisiPrihOdh')
+const gumbZapriBrisanje = document.querySelector('.gumbZapriBrisanje')
+
 
 let today = new Date();
 let dan1 = today.getDay()
 let mesec1 = today.getMonth() + 1
 let leto1 = today.getFullYear()
 
-//console.log('dela')
+//Dodajanje prihod in odhoda
 
 gumbi.addEventListener('click', (e) => {
-    console.log(e.target)
+    
     if(e.target.classList.contains('prihod')) {
         dodajPrihod('prihod')
     }
@@ -34,31 +32,6 @@ gumbi.addEventListener('click', (e) => {
         dodajPrihod('odhod')
     }
 })
-
-gumbZapri.addEventListener('click', () => {
-    obvestilo.classList.remove('prikazi')
-})
-
-zapriTabelo.addEventListener('click', () => {
-    tabelaOkvir.classList.remove('prikaziTabelo')
-})
-
-prikaziTabelo.addEventListener('click', () => {
-    tabelaOkvir.classList.add('prikaziTabelo')
-    tabelaPodatki(mesec1)
-})
-
-
-
-//fetch('http://localhost:4000/podatki')
-//.then(response => response.json())
-//.then(data => console.log(data))
-
-let podatki = null
-
-let jsona = JSON.stringify(podatki)
-
-console.log(jsona)
 
 const dodajPrihod = async (prihodOdhod) => {
     let d = new Date()
@@ -72,9 +45,8 @@ const dodajPrihod = async (prihodOdhod) => {
         mesec: Number(d.getMonth()) + 1,
         leto: d.getFullYear()
     }
-    console.log(pod.ura)
     try {
-    const post = await fetch('https://urca1.onrender.com/posljiPodatke', {
+    const post = await fetch('https://urca1.onrender/posljiPodatke', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -83,13 +55,10 @@ const dodajPrihod = async (prihodOdhod) => {
     })
     const response = await post.text()
     if(post.ok) {
-        console.log(response)
         obvestiloText.innerText = response
         obvestilo.classList.add('prikazi')
-        
     }
     if(!post.ok) {
-        console.log(response)
         obvestiloText.innerText = response
         obvestilo.classList.add('prikazi')
     }
@@ -97,17 +66,147 @@ const dodajPrihod = async (prihodOdhod) => {
     catch (e) {
         return e
     } 
-    //const response = await post.json()
-    //console.log(response)
     
     osveziStanje()    
 }
 
-const pridobiPodatke = async () => {
-    const response = await fetch('https://urca1.onrender.com/podatki')
+//Tabela
+
+zapriTabelo.addEventListener('click', () => {
+    tabelaOkvir.classList.remove('prikaziTabelo')
+})
+
+prikaziTabelo.addEventListener('click', () => {
+    tabelaOkvir.classList.add('prikaziTabelo')
+    tabelaPodatki(mesec1)
+})
+
+let podatki = null
+
+//let jsona = JSON.stringify(podatki)
+
+const tabelaPodatki = async (mesecIzbran) => {
+    tabela.innerHTML = 
+    `<tr>
+        <th>Datum</th>
+        <th>Prihod</th>
+        <th>Odhod</th>
+    </tr>`
+
+    let d = new Date
+    const pod = {
+        mesec: Number(mesecIzbran),
+        leto: d.getFullYear()
+    }
+    const response = await fetch('https://urca1.onrender/tabela?' + new URLSearchParams({
+        mesec: pod.mesec,
+        leto: pod.leto
+    }))
     const data = await response.json()
-    console.log(data)
+
+    podatki = data
+
+    for(const podatek of podatki) {
+        const tr = document.createElement('tr')
+        const td1 = document.createElement('td')
+        td1.innerText = podatek.datum
+        const td2 = document.createElement('td')
+        td2.setAttribute('id', podatek.prihod_id)
+        td2.innerText = podatek.ura_prihoda
+        const td3 = document.createElement('td')
+        td3.setAttribute('id', podatek.odhod_id)
+        td3.innerText = podatek.ura_odhoda
+        const td4 = document.createElement('td')
+        const bt = document.createElement('button')
+        bt.innerText = 'Briši'
+        bt.classList.add('gumb')
+        bt.classList.add('brisi')
+        td4.appendChild(bt)
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+        tr.appendChild(td4)
+        tabela.appendChild(tr)
+    }
 }
+
+mesci.addEventListener('change', (e) => {
+    let izrbanMesec = e.target.value
+    tabelaPodatki(izrbanMesec)
+})
+
+let idPrihod = null
+let idOdhod = null
+
+tabela.addEventListener('click', (e) => {
+    if(e.target.classList.contains('brisi')) {
+
+        idPrihod = e.target.parentElement.parentElement.children[1].id
+        idOdhod = e.target.parentElement.parentElement.children[2].id
+
+        okvirBrisiPrihOdh.style.display = 'block'
+
+        if(idOdhod === 'null') {
+            gumbOdhod.disabled = true
+        }
+        if(idPrihod === 'null') {
+            gumbPrihod.disabled = true
+        }
+        
+    }
+})
+
+//Brisanje podatka
+
+const brisiPodatek = async (id) => {
+    const data = await fetch('https://urca1.onrender/brisanje?' + new URLSearchParams({
+        id: id
+    }), { method: 'DELETE' })
+    
+    if(data.ok) {
+        const response = await data.text()
+        obvestiloText.innerText = response
+        obvestilo.classList.add('prikazi')
+    }
+    else {
+        console.log('Prišlo je do napake pri brisanju')
+    }
+}
+
+okvirBrisiPrihOdh.addEventListener('click',async (e) => {
+
+    if(e.target.classList.contains('gumbPrihod')) {
+        try {
+            await brisiPodatek(idPrihod)
+            tabelaPodatki(1)
+            okvirBrisiPrihOdh.style.display = 'none'
+        }
+        catch (e) {
+            console.log(e)
+        }
+        
+        
+    }
+    if(e.target.classList.contains('gumbOdhod')) {
+        await brisiPodatek(idOdhod)
+        tabelaPodatki(1)
+    
+        try {
+            await brisiPodatek(idOdhod)
+            tabelaPodatki(1)
+            okvirBrisiPrihOdh.style.display = 'none'
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
+    if(e.target.classList.contains('gumbZapriBrisanje')) {
+        okvirBrisiPrihOdh.style.display = 'none'
+    }
+})
+
+//Prikaz stanja
 
 const stanje = async () => {
     let d = new Date
@@ -138,129 +237,19 @@ const stanje = async () => {
     }
     return (ure + ':' + minute + ':' + sekunde)
 }
-
-const tabelaPodatki = async (mesecIzbran) => {
-    tabela.innerHTML = `<tr>
-    <th>Datum</th>
-    <th>Prihod</th>
-    <th>Odhod</th>
-</tr>`
-
-    let d = new Date
-    const pod = {
-        mesec: Number(mesecIzbran),
-        leto: d.getFullYear()
-    }
-    const response = await fetch('https://urca1.onrender.com/tabela?' + new URLSearchParams({
-        mesec: pod.mesec,
-        leto: pod.leto
-    }))
-    const data = await response.json()
-    console.log(data)
-    podatki = data
-    for(const podatek of podatki) {
-        const tr = document.createElement('tr')
-        const td1 = document.createElement('td')
-        td1.innerText = podatek.datum
-        const td2 = document.createElement('td')
-        td2.setAttribute('id', podatek.prihod_id)
-        td2.innerText = podatek.ura_prihoda
-        const td3 = document.createElement('td')
-        td3.setAttribute('id', podatek.odhod_id)
-        td3.innerText = podatek.ura_odhoda
-        const td4 = document.createElement('td')
-        const bt = document.createElement('button')
-        bt.innerText = 'Briši'
-        bt.classList.add('gumb')
-        //bt.classList.add('btn-primary')
-        bt.classList.add('brisi')
-        td4.appendChild(bt)
-        tr.appendChild(td1)
-        tr.appendChild(td2)
-        tr.appendChild(td3)
-        tr.appendChild(td4)
-        tabela.appendChild(tr)
-
-    }
-}
-
-
-
 const osveziStanje = async () => {
     let novoStanje = await stanje()
     stanjePrikaz.innerText = novoStanje
 }
 
-osveziStanje()
-
 document.addEventListener('DOMContentLoaded', () => {
     osveziStanje()
 })
 
-mesci.addEventListener('change', (e) => {
-    console.log(e.target.value)
-    let izrbanMesec = e.target.value
-    tabelaPodatki(izrbanMesec)
-})
+//Zapri obvestilo
 
-let idPrihod = null
-let idOdhod = null
-
-tabela.addEventListener('click', (e) => {
-    if(e.target.classList.contains('brisi')) {
-
-        idPrihod = e.target.parentElement.parentElement.children[1].id
-        idOdhod = e.target.parentElement.parentElement.children[2].id
-
-        okvirBrisiPrihOdh.style.display = 'block'
-        
-    }
-})
-
-const brisiPodatek = async (id) => {
-    const data = await fetch('https://urca1.onrender.com/brisanje?' + new URLSearchParams({
-        id: id
-    }), { method: 'DELETE' })
-    
-    if(data.ok) {
-        const response = await data.text()
-        obvestiloText.innerText = response
-        obvestilo.classList.add('prikazi')
-    }
-    else {
-        console.log('Prišlo je do napake pri brisanju')
-    }
-}
-
-
-gumbaBrisanje.addEventListener('click',async (e) => {
-    console.log(e.target)
-    console.log(idPrihod)
-    console.log(idOdhod)
-
-    if(e.target.classList.contains('gumbPrihod')) {
-        try {
-            await brisiPodatek(idPrihod)
-            tabelaPodatki(1)
-            okvirBrisiPrihOdh.style.display = 'none'
-        }
-        catch (e) {
-            console.log(e)
-        }
-    }
-    if(e.target.classList.contains('gumbOdhod')) {
-        await brisiPodatek(idOdhod)
-        tabelaPodatki(1)
-    
-        try {
-            await brisiPodatek(idOdhod)
-            tabelaPodatki(1)
-            okvirBrisiPrihOdh.style.display = 'none'
-        }
-        catch (e) {
-            console.log(e)
-        }
-    }
+gumbZapriObvestilo.addEventListener('click', () => {
+    obvestilo.classList.remove('prikazi')
 })
 
 
